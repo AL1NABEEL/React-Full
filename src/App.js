8261922185e6e30ee6f608e53e24596af727aa8d
Ali Nabeel <alinabeel.1980@gmail.com>
2023-04-13 06:28:07 +0300

    added the route thing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About';
 function App() {
   return (
   <div>
-  <Home/>
+  <Navbar/>
+  <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<div>404 NOT FOUND</div>} />
+      </Routes>
+  {/* <Home/> */}
   </div>
 );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,14 @@
-// import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <header>
+        <h1 className='navbarh1'>hello</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+    </header>
+  );
+}
+
+export default Navbar;
